3248161941a21cadaec83978811a6d058b9bad57
Nurrohmann <nurrohmanmarduki@gmail.com>
2024-06-15 18:51:01 +0700

    chore: add react icons

--- src/components/ui/card.jsx
+++ src/components/ui/card.jsx
@@ -13,12 +13,14 @@ import axios from "axios";
 export function Card({ name, imgUrl }) {
   const [openModal, setOpenModal] = useState(false);
   const [imageLoaded, setImageLoaded] = useState(false);
+  const [pokemonData, setPokemonData] = useState();
 
   const getDetailPokemon = async () => {
     try {
       const { data: pokemonDetail } = await axios(
         `https://pokeapi.co/api/v2/pokemon/${name}`
       );
+      setPokemonData(pokemonDetail);
     } catch (error) {
       console.log(error);
     }
@@ -29,6 +31,8 @@ export function Card({ name, imgUrl }) {
       getDetailPokemon();
     }
   }, [openModal]);
+
+  console.log(pokemonData);
 
   return (
     <Dialog open={openModal} onOpenChange={setOpenModal}>
@@ -46,13 +50,23 @@ export function Card({ name, imgUrl }) {
         </div>
       </DialogTrigger>
       <DialogContent>
-        <DialogHeader>
-          <DialogTitle>Are you absolutely sure?</DialogTitle>
-          <DialogDescription>
-            This action cannot be undone. This will permanently delete your
-            account and remove your data from our servers.
-          </DialogDescription>
-        </DialogHeader>
+        <div>
+          <h1>{name}</h1>
+          <img className="h-[180px] w-[180px]" src={imgUrl} alt="" />
+          <p>Exp: {pokemonData?.base_experience}</p>
+          <p>
+            Type:{" "}
+            {pokemonData?.types.map((item, index) => (
+              <span key={index}>
+                {item.type.name} {"   "}
+              </span>
+            ))}
+          </p>
+          <p>Hp: {pokemonData?.stats[0].base_stat}</p>
+          <p>Attack: {pokemonData?.stats[1].base_stat}</p>
+          <p>Defense: {pokemonData?.stats[2].base_stat}</p>
+          <p>Speed: {pokemonData?.stats[5].base_stat}</p>
+        </div>
       </DialogContent>
     </Dialog>
   );
