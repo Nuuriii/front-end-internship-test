62010cbfa57645ee0d13a9e655581c221f3fe1ef
Nurrohmann <nurrohmanmarduki@gmail.com>
2024-06-16 10:34:44 +0700

    feat: error state if failed fetching data

--- src/components/element/listCard.jsx
+++ src/components/element/listCard.jsx
@@ -5,17 +5,33 @@ import { useState, useEffect } from "react";
 export function ListCard() {
   const [limit, setLimit] = useState(20);
   const [listPokemon, setListPokemon] = useState([]);
-  const [loading, setLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
+  const [loading, setLoading] = useState({
+    firstLoadLoading: false,
+    loadMoreLoading: false,
+  });
 
   const getPokemon = async () => {
+    setLoading({
+      firstLoadLoading: true,
+      loadMoreLoading: listPokemon.length > 0,
+    });
     try {
       const { data: getData } = await axios(
         `https://pokeapi.co/api/v2/pokemon?limit=${limit}`
       );
       setListPokemon(getData.results);
-      setLoading(false);
+      setLoading({
+        firstLoadLoading: false,
+        loadMoreLoading: false,
+      });
+      setIsError(false);
     } catch (error) {
-      setLoading(false);
+      setIsError(true);
+      setLoading({
+        firstLoadLoading: false,
+        loadMoreLoading: false,
+      });
       console.log(error);
     }
   };
@@ -31,26 +47,34 @@ export function ListCard() {
 
   return (
     <div className="py-[30px]">
-      <ul className="grid sm2:grid-cols-2 sm4:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 justify-items-center gap-9">
-        {listPokemon?.map((item, index) => (
-          <li
-            className="w-full sm1:w-[220px] sm2:w-[150px] sm3:w-[184px] shadow-[rgba(0,_0,_0,_0.24)_0px_3px_8px] rounded-md p-0 hover:bg-neutral-100 hover:scale-110 transition-all duration-200"
-            key={index}
-          >
-            <Card
-              name={item.name}
-              imgUrl={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${
-                index + 1
-              }.png`}
-            />
-          </li>
-        ))}
-      </ul>
-      <div className="flex justify-center mt-[30px]">
-        <Button disabled={loading} onClick={handleLoadMore}>
-          {loading ? "Loading. . ." : "Load More"}
-        </Button>
-      </div>
+      {loading.firstLoadLoading ? (
+        <h1 className="text-center">Loading. . .</h1>
+      ) : isError ? (
+        <h1 className="text-center">Gagal mengambil daftar Pokemon</h1>
+      ) : (
+        <>
+          <ul className="grid sm2:grid-cols-2 sm4:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 justify-items-center gap-9">
+            {listPokemon?.map((item, index) => (
+              <li
+                className="w-full sm1:w-[220px] sm2:w-[150px] sm3:w-[184px] shadow-[rgba(0,_0,_0,_0.24)_0px_3px_8px] rounded-md p-0 hover:bg-neutral-100 hover:scale-110 transition-all duration-200"
+                key={index}
+              >
+                <Card
+                  name={item.name}
+                  imgUrl={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${
+                    index + 1
+                  }.png`}
+                />
+              </li>
+            ))}
+          </ul>
+          <div className="flex justify-center mt-[30px]">
+            <Button disabled={loading.loadMoreLoading} onClick={handleLoadMore}>
+              {loading.loadMoreLoading ? "Loading. . ." : "Load More"}
+            </Button>
+          </div>
+        </>
+      )}
     </div>
   );
 }
